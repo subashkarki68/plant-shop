a825295a5d0fe3e6401128f15a6fdde8e224b390
Subash Karki <subashkarki68@gmail.com>
2024-06-14 18:37:45 +0545

    "Updated App.js to reorganize imports and routes, and Api.js to use environment variable for backend URL"

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,55 +1,57 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/navbar/Navbar";
+import About from "./pages/about/About";
+import AdminDashboard from "./pages/admin/AdminDashboard/AdminDashboard";
+import AdminOrders from "./pages/admin/AdminOrders/AdminOrders";
+import AdminProductEdit from "./pages/admin/AdminProductEdit/AdminProductEdit";
+import Cart from "./pages/cart/Cart";
+import ForgotPassword from "./pages/forgot_password/ForgotPassword";
 import Homepage from "./pages/homepage/Homepage";
 import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import About from "./pages/about/About";
-import Navbar from "./components/navbar/Navbar";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import AdminDashboard from "./pages/admin/AdminDashboard/AdminDashboard";
-import AdminProductEdit from "./pages/admin/AdminProductEdit/AdminProductEdit";
-import ProductDetails from "./pages/productDetails/ProductDetails";
-import Cart from "./pages/cart/Cart";
 import Order from "./pages/orders/Order";
-import AdminOrders from "./pages/admin/AdminOrders/AdminOrders";
-import Search from "./pages/search/Search";
+import ProductDetails from "./pages/productDetails/ProductDetails";
 import Profile from "./pages/profile/Profile";
-import ForgotPassword from "./pages/forgot_password/ForgotPassword";
+import Register from "./pages/register/Register";
+import Search from "./pages/search/Search";
 
 function App() {
+  console.log(process.env);
   return (
     <>
       <Router>
         <Navbar />
         <ToastContainer />
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/product/details/:id" element={<ProductDetails />} />
+          <Route path='/' element={<Homepage />} />
+          <Route path='/product/details/:id' element={<ProductDetails />} />
 
           {/* cart */}
-          <Route path="/cart" element={<Cart />} />
+          <Route path='/cart' element={<Cart />} />
 
           {/* orders */}
-          <Route path="/order" element={<Order />} />
+          <Route path='/order' element={<Order />} />
 
-
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
 
           {/* about */}
-          <Route path="/about" element={<About />} />
+          <Route path='/about' element={<About />} />
 
           {/* Admin routes */}
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
-          <Route path="/admin/product/edit/:id" element={<AdminProductEdit />} />
-          <Route path="/admin/orders" element={<AdminOrders />} />
+          <Route path='/admin-dashboard' element={<AdminDashboard />} />
+          <Route
+            path='/admin/product/edit/:id'
+            element={<AdminProductEdit />}
+          />
+          <Route path='/admin/orders' element={<AdminOrders />} />
 
           {/* search */}
-          <Route path="/search/:query" element={<Search />} />
+          <Route path='/search/:query' element={<Search />} />
 
-          <Route path='/profile' element={<Profile/>} />
-          <Route path='/forgot_password' element={<ForgotPassword/>} />
-
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/forgot_password' element={<ForgotPassword />} />
         </Routes>
       </Router>
     </>
--- Frontend/src/apis/Api.js
+++ Frontend/src/apis/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "multipart/form-data",
